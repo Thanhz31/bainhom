49d1cadd36d118428bb6ebd76fd16d5558c20544
anh123q <le091488743@gmail.com>
2026-07-05 22:05:12 +0700

    up ảnh

--- views/dung_chung/admin_header.php
+++ views/dung_chung/admin_header.php
@@ -7,5 +7,5 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="bg-light">
+<body class="bg-light">    
 <div class="container mt-4 mb-5">
